e9bb1806222ddccb25adba5c8d89db874e95dd98
OuiMais <flohofbauer@icloud.com>
2023-08-17 08:52:53 +0200

    Update main.py by PyCharm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
     Projet : KingMods
     Date Creation : 07/08/2023
-    Date Revision : 10/08/2023
+    Date Revision : 16/08/2023
     Entreprise : 3SC4P3
     Auteur: Florian HOFBAUER
     Contact :
@@ -13,8 +13,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 import datetime
+import time
 # import csv
-# import time
 
 # Option for website (no screen open)
 options = Options()
@@ -23,44 +23,101 @@ options.add_argument('--headless')
 # Initiate the browser
 browser = webdriver.Chrome(options=options)
 
-tableauToPrint = ""
-nbPage = 1
-newDay = 0
+""" 
+    Cette partie permet de savoir si un nouveau mod vient de sortir
+"""
+import keyboard
 
-while not newDay:
-    print(nbPage)
-    # on charge la page souhaitee
-    url = "https://www.kingmods.net/fr/fs22/nouveaux-mods?page=" + str(nbPage)
+previousTitle = ""
+timeBetweenRefresh = 10.0
+programmRun = 1
+
+while programmRun:
+    url = "https://www.kingmods.net/fr/fs22/nouveaux-mod"
 
     browser.get(url)
 
     # on recupere toutes les donnees a partir des lignes html
     linked = browser.find_elements(By.TAG_NAME, "a")
 
-    dates = browser.find_elements(By.TAG_NAME, "time")
+    links = linked[58] # beaucoup de lignes permettant de naviguer sont presentes avant les mods
 
-    # Une oage contient 12 mods
-    for iteration in range(len(dates)):
-        date = dates[iteration]
-        links = linked[58 + iteration] # beaucoup de lignes permettant de naviguer sont presentes avant les mods
+    link = links.get_attribute("href") # lien du mod
+    titreMod = links.get_attribute("title") # titre
 
-        dateStr = date.get_attribute("datetime") # date
-        link = links.get_attribute("href") # lien du mod
-        titreMod = links.get_attribute("title") # titre
+    if titreMod != previousTitle:
+        print("We have a new mod on KingsMod : %s at the link %s" % (titreMod, link))
+        previousTitle = titreMod
 
-        dateDatetime = datetime.datetime.strptime(dateStr, "%Y-%m-%dT%H:%M:%SZ") # utilisation de datetime pour
-        # comparer des dates
-        nowDateTime = datetime.date.today() # - timedelta(days=1)
+    browser.quit()
+    time.sleep(timeBetweenRefresh)
 
-        if dateDatetime.date() == nowDateTime:
-            # preparation pour csv
-            dataToAdd = titreMod + ";" + link + ";\n"
-            tableauToPrint += dataToAdd
-        else:
-            newDay = 1
+    if keyboard.is_pressed("esc") or time.time() % 86400 != 0:
+        # si on interrompt le programme avec la touche échap, on arrête tout, sinon c'est qu'il est minuit donc on
+        # fait un bilan de la journée
+        programmRun = 0
 
-    nbPage += 1
+        """ 
+            Cette partie permet de connaître le nombre de nouveaux mod de la veille et d'obtenir un fichier avec le nom 
+            et le lien de chaque mod
+        """
 
-browser.quit()
+        if time.time() % 86400 == 0:
+            time.sleep(1.0)
+            tableauToPrint = ""
+            nbPage = 1
+            newDay = 0
+            numberOfModsYesterday = 0
 
-print(tableauToPrint)
+            while not newDay:
+                print(nbPage)
+                # on charge la page souhaitee
+                url = "https://www.kingmods.net/fr/fs22/nouveaux-mods?page=" + str(nbPage)
+
+                browser.get(url)
+
+                # on recupere toutes les donnees a partir des lignes html
+                linked = browser.find_elements(By.TAG_NAME, "a")
+
+                dates = browser.find_elements(By.TAG_NAME, "time")
+
+                # Une page contient 12 mods
+                for iteration in range(len(dates)):
+                    date = dates[iteration]
+                    links = linked[
+                        58 + iteration]  # beaucoup de lignes permettant de naviguer sont presentes avant les mods
+
+                    dateStr = date.get_attribute("datetime")  # date
+                    link = links.get_attribute("href")  # lien du mod
+                    titreMod = links.get_attribute("title")  # titre
+
+                    dateDatetime = datetime.datetime.strptime(dateStr,
+                                                              "%Y-%m-%dT%H:%M:%SZ")  # utilisation de datetime pour
+                    # comparer des dates
+                    nowDateTime = datetime.date.today()
+                    yesterdayDateTime = nowDateTime - datetime.timedelta(days=1)
+
+                    if dateDatetime.date() == nowDateTime:
+                        pass
+                    elif dateDatetime.date() == yesterdayDateTime:
+                        # preparation pour csv
+                        dataToAdd = titreMod + ";" + link + ";\n"
+                        tableauToPrint += dataToAdd
+                        numberOfModsYesterday += 1
+                    else:
+                        newDay = 1
+
+                nbPage += 1
+
+            browser.quit()
+
+            print(tableauToPrint)
+            print("%s Mods add yesterday!!" % numberOfModsYesterday)
+            programmRun = 1
+
+"""
+    To add: 
+        - Faire un bot qui envoie les messages de nouveau mods et le récapitulatif
+        - Mettre les mods dans un fichier csv pour faciliter la lecture
+        - 
+"""
