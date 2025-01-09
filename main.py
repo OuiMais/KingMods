144d2354e4b1629a9ccb0eff1951ee4359d49877
OuiMais <flohofbauer@icloud.com>
2025-01-09 21:50:35 +0100

    No image in the excel because it's not what i want

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
     Projet : KingMods
     Date Creation : 07/08/2023
-    Date Revision : 08/01/2025
+    Date Revision : /01/2025
     Entreprise : 3SC4P3
     Auteur: Florian HOFBAUER
     Contact :
@@ -11,22 +11,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
 
 import datetime
 import time
-import csv
 
-from selenium.webdriver.support.wait import WebDriverWait
-import io
-from PIL import Image
+from openpyxl import Workbook
 
 # Option for website (no screen open)
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 
-previousTitle = "US Farmer"
+previousTitle = "Pack d'outils pour pierres"
 url = "https://www.kingmods.net/fr/fs25/nouveaux-mods"
 
 # Set time between data refresh
@@ -54,14 +50,6 @@ while not findPrevious:
     lastPageMod = pageModList.find_elements(By.TAG_NAME, 'a')
 
     for mod in lastPageMod:
-        test = mod.find_element(By.TAG_NAME, 'img')
-        img = test.get_attribute('src')
-        browserImage.get(img)
-        ima = browserImage.find_element(By.TAG_NAME, 'img').screenshot_as_png
-
-        image = Image.open(io.BytesIO(ima))
-        image.show()
-
         try:
             # Check if badge update is display
             badge = mod.find_element(By.XPATH, ".//div[contains(@class, 'bg-blue')]")
@@ -96,135 +84,97 @@ for mod in modTitleAndLink:
         seen.add(key)
         filteredMod.append(mod)
 
-newModCsv = open("NewModResume.csv", 'w', newline='', encoding='utf-8')
-updateModCsv = open("NewModResume.csv", 'w', newline='', encoding='utf-8')
+excelFile = Workbook()
 
-newWriter = csv.writer(newModCsv, delimiter=";")
-updateWriter = csv.writer(updateModCsv, delimiter=";")
+newModSheet = excelFile.active
+newModSheet.title = "New Mod"
+updateModSheet = excelFile.create_sheet(title="Update Mod")
 
-newWriter.writerow(['Title', 'Link'])
-updateWriter.writerow(['Title', 'Link'])
+newModSheet.append(["Title", "Link"])
+updateModSheet.append(["Title", "Link"])
 
-for mod in filteredMod:
-    data = [mod[0], mod[1]]
+for index, mod in enumerate(filteredMod, start=2):
+    title = mod[0]
+    link = mod[1]
+
     if mod[2]:
-        updateWriter.writerow(data)
+        updateModSheet[f"A{index}"] = title
+        updateModSheet[f"B{index}"] = link
     else:
-        newWriter.writerow(data)
+        newModSheet[f"A{index}"] = title
+        newModSheet[f"B{index}"] = link
 
-newModCsv.close()
-updateModCsv.close()
-
-"""
-modification pour ajouter les images sur le excel
-
-from openpyxl import Workbook
-from openpyxl.drawing.image import Image
-import os
-
-# Exemple de données : liste des mods avec titre, lien et chemin de l'image
-mods = [
-    ["Mod 1", "https://example.com/mod1", "mod1.png"],
-    ["Mod 2", "https://example.com/mod2", "mod2.png"],
-    ["Mod 3", "https://example.com/mod3", "nonexistent_image.png"],  # Image inexistante
-]
-
-# Création du fichier Excel
-wb = Workbook()
-ws = wb.active
-
-# Ajouter les en-têtes
-ws.append(["Title", "Link", "Image"])
-
-# Ajouter chaque mod au fichier Excel
-for index, mod in enumerate(mods, start=2):  # Start=2 car la première ligne est pour les en-têtes
-    title, link, image_path = mod
-
-    # Ajouter le titre et le lien dans les colonnes A et B
-    ws[f"A{index}"] = title
-    ws[f"B{index}"] = link
-
-    # Ajouter l'image si elle existe
-    if os.path.exists(image_path):  # Vérifie si l'image existe
-        img = Image(image_path)
-        ws.add_image(img, f"C{index}")  # Place l'image dans la colonne C
-    else:
-        ws[f"C{index}"] = "Image not found"  # Indique que l'image est introuvable
-
-# Sauvegarder le fichier Excel
-wb.save("mods_with_images.xlsx")
-print("Fichier Excel avec images créé.")
+excelFile.save("KingsMod_Resume.xlsx")
 
 
-"""
-    # timeEndDay = time.time() % 86400
-    #
-    # if keyboard.is_pressed("esc") or timeEndDay == 0:
-    #     # si on interrompt le programme avec la touche échap, on arrête tout, sinon c'est qu'il est minuit donc on
-    #     # fait un bilan de la journée
-    #     programmRun = 0
-    #
-    #     """
-    #         Cette partie permet de connaître le nombre de nouveaux mod de la veille et d'obtenir un fichier avec le nom
-    #         et le lien de chaque mod
-    #     """
-    #
-    #     if timeEndDay == 0:
-    #         time.sleep(1.0)
-    #         tableauToPrint = ""
-    #         nbPage = 1
-    #         newDay = 0
-    #         numberOfModsYesterday = 0
-    #
-    #         while not newDay:
-    #             print(nbPage)
-    #             # on charge la page souhaitee
-    #             url = "https://www.kingmods.net/fr/fs22/nouveaux-mods?page=" + str(nbPage)
-    #
-    #             browser.get(url)
-    #
-    #             # on recupere toutes les donnees a partir des lignes html
-    #             linked = browser.find_elements(By.TAG_NAME, "a")
-    #
-    #             dates = browser.find_elements(By.TAG_NAME, "time")
-    #
-    #             # Une page contient 12 mods
-    #             for iteration in range(len(dates)):
-    #                 date = dates[iteration]
-    #                 links = linked[
-    #                     58 + iteration]  # beaucoup de lignes permettant de naviguer sont presentes avant les mods
-    #
-    #                 dateStr = date.get_attribute("datetime")  # date
-    #                 link = links.get_attribute("href")  # lien du mod
-    #                 titreMod = links.get_attribute("title")  # titre
-    #
-    #                 dateDatetime = datetime.datetime.strptime(dateStr,
-    #                                                           "%Y-%m-%dT%H:%M:%SZ")  # utilisation de datetime pour
-    #                 # comparer des dates
-    #                 nowDateTime = datetime.date.today()
-    #                 yesterdayDateTime = nowDateTime - datetime.timedelta(days=1)
-    #
-    #                 if dateDatetime.date() == nowDateTime:
-    #                     pass
-    #                 elif dateDatetime.date() == yesterdayDateTime:
-    #                     # preparation pour csv
-    #                     dataToAdd = titreMod + ";" + link + ";\n"
-    #                     tableauToPrint += dataToAdd
-    #                     numberOfModsYesterday += 1
-    #                 else:
-    #                     newDay = 1
-    #
-    #             nbPage += 1
-    #
-    #         browser.quit()
-    #
-    #         print(tableauToPrint)
-    #         print("%s Mods add yesterday!!" % numberOfModsYesterday)
-    #         programmRun = 1
+# timeEndDay = time.time() % 86400
+#
+# if keyboard.is_pressed("esc") or timeEndDay == 0:
+#     # si on interrompt le programme avec la touche échap, on arrête tout, sinon c'est qu'il est minuit donc on
+#     # fait un bilan de la journée
+#     programmRun = 0
+#
+#     """
+#         Cette partie permet de connaître le nombre de nouveaux mod de la veille et d'obtenir un fichier avec le nom
+#         et le lien de chaque mod
+#     """
+#
+#     if timeEndDay == 0:
+#         time.sleep(1.0)
+#         tableauToPrint = ""
+#         nbPage = 1
+#         newDay = 0
+#         numberOfModsYesterday = 0
+#
+#         while not newDay:
+#             print(nbPage)
+#             # on charge la page souhaitee
+#             url = "https://www.kingmods.net/fr/fs22/nouveaux-mods?page=" + str(nbPage)
+#
+#             browser.get(url)
+#
+#             # on recupere toutes les donnees a partir des lignes html
+#             linked = browser.find_elements(By.TAG_NAME, "a")
+#
+#             dates = browser.find_elements(By.TAG_NAME, "time")
+#
+#             # Une page contient 12 mods
+#             for iteration in range(len(dates)):
+#                 date = dates[iteration]
+#                 links = linked[
+#                     58 + iteration]  # beaucoup de lignes permettant de naviguer sont presentes avant les mods
+#
+#                 dateStr = date.get_attribute("datetime")  # date
+#                 link = links.get_attribute("href")  # lien du mod
+#                 titreMod = links.get_attribute("title")  # titre
+#
+#                 dateDatetime = datetime.datetime.strptime(dateStr,
+#                                                           "%Y-%m-%dT%H:%M:%SZ")  # utilisation de datetime pour
+#                 # comparer des dates
+#                 nowDateTime = datetime.date.today()
+#                 yesterdayDateTime = nowDateTime - datetime.timedelta(days=1)
+#
+#                 if dateDatetime.date() == nowDateTime:
+#                     pass
+#                 elif dateDatetime.date() == yesterdayDateTime:
+#                     # preparation pour csv
+#                     dataToAdd = titreMod + ";" + link + ";\n"
+#                     tableauToPrint += dataToAdd
+#                     numberOfModsYesterday += 1
+#                 else:
+#                     newDay = 1
+#
+#             nbPage += 1
+#
+#         browser.quit()
+#
+#         print(tableauToPrint)
+#         print("%s Mods add yesterday!!" % numberOfModsYesterday)
+#         programmRun = 1
 
 """
     To add: 
         - Faire un bot qui envoie les messages de nouveau mods et le récapitulatif
-        - Mettre les mods dans un fichier csv pour faciliter la lecture
+        - Comment save le previous mod pour un raspberry pi 
         - 
 """
